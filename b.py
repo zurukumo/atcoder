b0e0fa7751e448007179c73dfa4a656fa93b4852
Matsuda Shinji <zurukumo@gmail.com>
2019-11-04 20:39:19 +0900

    jsc2019-qual C trying

--- b.py
+++ b.py
@@ -1,10 +1,36 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
-
+from collections import deque
 N = int(input())
 S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-A = [int(input()) for _ in range(N)]
+
+mod = 10 ** 9 + 7
+
+def solve() :
+    if S[0] != 'B' or S[-1] != 'B' :
+        return 0
+        
+    block = deque([])
+    c = 1
+    for i in range(1, 2 * N) :
+        if S[i] == S[i-1] :
+            c += 1
+        else :
+            block.append(c)
+            c = 1
+    block.append(c)
+            
+    ret = 1
+    for i in range(1, N + 1) :
+        ret *= i * i
+        ret %= mod
+    
+    while len(block) > 1 :
+        a = block.popleft()
+        b = block.pop()
+        if (a + b) % 2 != 0 :
+            return 0
+        ret *= pow((a + b) // 2, mod - 2, mod)
+        ret %= mod
+        
+    return ret
+        
+print(solve())
