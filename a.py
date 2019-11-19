fbc4a7dcb3f82712e85fe1aef561d57d1a7b7872
Matsuda Shinji <zurukumo@gmail.com>
2019-11-19 20:12:32 +0900

    solved ARC098 E

--- a.py
+++ a.py
@@ -1,25 +1,10 @@
-# T = int(input())
-# ABCD = [[int(i) for i in input().split()] for _ in range()]
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
 
-from random import randint
-n = 0
-while n < 50 :
-    A, B, C, D = (randint(1, 20) for _ in range(4))
-    # A, B, C, D = 9, 7, 6, 9
-
-    # print(A, B, C, D)
-    origin = [A, B, C, D]
-    
-    count = 0
-    while A >= B and count < 1000 :
-        A -= B
-        # print('Morning', A)
-        if A <= C :
-            A += D
-            # print('Evening', A)
-        count += 1
-        
-    if count == 1000 :
-        print(origin)
-        n += 1
-    # break
+N = int(input())
+S = input()
+N, K = map(int, input().split())
+xy = [[int(i) for i in input().split()] for _ in range(N)]
+x = [int(i) for i in input().split()]
+A = [int(input()) for _ in range(N)]
