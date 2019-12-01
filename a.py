fdc4431914ac5fee470b11b530db58b4aa504760
Matsuda Shinji <zurukumo@gmail.com>
2019-12-01 19:44:40 +0900

    ARC035 C trying

--- a.py
+++ a.py
@@ -1,10 +1,23 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
+N, M = map(int, input().split())
+vec = [[float('inf')] * N for _ in range(N)]
+for _ in range(M) :
+    A, B, C = map(int, input().split())
+    vec[A-1][B-1] = C
+    vec[B-1][A-1] = C
 
-N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-A = [int(input()) for _ in range(N)]
+for i in range(N) :
+    vec[i][i] = 0
+
+# ワーシャルフロイド
+for k in range(N) :
+    for i in range(N) :
+        for j in range(N) :
+            vec[i][j] = min(vec[i][j], vec[i][k] + vec[k][j])
+
+for i in range(N) :
+    print(vec[i])
+
+K = int(input())
+for _ in range(K) :
+    X, Y, Z = map(int, input().split())
+
