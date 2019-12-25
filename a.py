ba64ed2dd5d6757e3c6b025a8ffe79d28ab66c2b
mshinji <zurukumo@gmail.com>
2019-12-25 16:16:43 +0900

    'update'

--- a.py
+++ a.py
@@ -1,10 +1,17 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
+A, B = map(int, input().split())
 
-N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-A = [int(input()) for _ in range(N)]
+def f(X) :
+  X = [int(i) for i in str(X)]
+  a, b = 0, 1
+  for x in X :
+    if 4 < x <= 9 :
+      a = a * 8 + b * (x - 1)
+    else :
+      a = a * 8 + b * x
+    
+    if x == 4 or x == 9 :
+      b = 0
+      
+  return a + b
+
+print(B - (A - 1) - (f(B) - f(A - 1)))
