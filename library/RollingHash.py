da406c730ed11334ee4363f1e3c022a6851f329a
mshinji <zurukumo@gmail.com>
2020-01-18 20:46:35 +0900

    update: library/RollingHash.py

--- library/RollingHash.py
+++ library/RollingHash.py
@@ -1,15 +1,15 @@
-# ローリングハッシュ
 class RollingHash() :
-    def __init__(self, S, base=26, mod=2**61-1) :
-        hash = [0]
-        pow = [1]
-        for s in S :
-            hash.append((hash[-1] * base + ord(s)) % mod)
-            pow.append(pow[-1] * base % mod)
-
-        self.hash = hash
-        self.pow = pow
+    # mod ex. (1<<61)-1, (1<<31)-1
+    # 文字列のときはxをordに通すのを忘れない！！
+    def __init__(self, x, base=26, mod=(1<<61)-1) :
+        self.hash = [0]
+        self.pow = [1]
         self.mod = mod
         
+        for xi in x :
+            self.hash.append((self.hash[-1] * base + xi) % mod)
+            self.pow.append(self.pow[-1] * base % mod)
+            
+    # [l, r)
     def get(self, l, r) :
-        return (self.hash[r+1] - self.hash[l] * self.pow[r+1-l]) % self.mod
+        return (self.hash[r] - self.hash[l] * self.pow[r-l]) % self.mod
