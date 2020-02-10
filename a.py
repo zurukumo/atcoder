d20fda967379ed54470f7b2ef49595b21a7cc498
zurukumo <zurukumo@gmail.com>
2020-02-10 19:14:32 +0900

    update: atcoder.txt

--- a.py
+++ a.py
@@ -1,22 +1,11 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
 N = int(input())
-
-ret = 0
-for _ in range(N) :
-  X, Y, Z = map(int, input().split())
-  M = int(input())
-  seq = [float('inf')] * 6
-  for _ in range(M) :
-    x, y, z = map(int, input().split())
-    seq[0] = min(seq[0], x)
-    seq[1] = min(seq[1], X - (x + 1))
-    seq[2] = min(seq[2], y)
-    seq[3] = min(seq[3], Y - (y + 1))
-    seq[4] = min(seq[4], z)
-    seq[5] = min(seq[5], Z - (z + 1))
-  for i in range(6) :
-    ret ^= seq[i]
-
-if ret != 0 :
-  print('WIN')
-else :
-  print('LOSE')
+S = input()
+N, K = map(int, input().split())
+xy = [[int(i) for i in input().split()] for _ in range(N)]
+x = [int(i) for i in input().split()]
+S = [input() for _ in range(N)]
+A = [int(input()) for _ in range(N)]
