7ffe024efca2538462e2d51536bd38ed976d5f14
zurukumo <zurukumo@gmail.com>
2025-01-28 10:37:24 +0900

    feat: CLIでテストケースを指定できるように変更

--- clis/test.py
+++ clis/test.py
@@ -1,28 +1,43 @@
 import argparse
 import subprocess
+from typing import Generator
 
 import colorama
+
+
+def read_testcase_file(problem, testcase) -> tuple[str, str]:
+    with open(f"tests/{problem}/{testcase}-in.txt") as f:
+        input = f.read()
+    with open(f"tests/{problem}/{testcase}-out.txt") as f:
+        expected = f.read()
+
+    if input == "":
+        raise FileNotFoundError
+
+    return input, expected
+
+
+def testcase_generator(
+    problem, testcase: int | None = None
+) -> Generator[tuple[int, tuple[str, str]], None, None]:
+    if testcase is not None:
+        yield testcase, read_testcase_file(problem, testcase)
+    else:
+        for i in range(1, 1000):
+            try:
+                yield i, read_testcase_file(problem, i)
+            except FileNotFoundError:
+                break
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("problem", type=str, help="The problem to test")
+    parser.add_argument("--testcase", type=int, help="The testcase to run")
 
     args = parser.parse_args()
 
-    for i in range(1, 1000):
-        try:
-            with open(f"tests/{args.problem}/{i}-in.txt") as f:
-                input = f.read()
-            with open(f"tests/{args.problem}/{i}-out.txt") as f:
-                expected = f.read()
-
-            # inputが空の場合break
-            if input == "":
-                break
-        except FileNotFoundError:
-            break
-
+    for i, (input, expected) in testcase_generator(args.problem, args.testcase):
         result = subprocess.run(
             ["python3", f"problems/{args.problem}.py"],
             input=input.encode(),
