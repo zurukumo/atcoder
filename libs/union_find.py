db2427fea59bd9fea06a088705bd2344cca0c2ec
zurukumo <zurukumo@gmail.com>
2025-01-30 13:02:04 +0900

    feat: UnionFindのsizeをheightに置き換え、新たにsizeメソッドを追加

--- libs/union_find.py
+++ libs/union_find.py
@@ -1,11 +1,12 @@
 class UnionFind:
     def __init__(self, N):
-        self.parent = [-1] * N
+        self.parents = [-1] * N
+        self.sizes = [1] * N
         self.units = N
 
     def root(self, x):
-        while self.parent[x] >= 0:
-            x = self.parent[x]
+        while self.parents[x] >= 0:
+            x = self.parents[x]
         return x
 
     def unite(self, x, y):
@@ -14,29 +15,34 @@ class UnionFind:
 
         if rx != ry:
             self.units -= 1
-            if self.parent[rx] > self.parent[ry]:
-                self.parent[rx] = ry
+            if self.parents[rx] > self.parents[ry]:
+                self.parents[rx] = ry
+                self.sizes[ry] += self.sizes[rx]
             else:
-                if self.parent[rx] == self.parent[ry]:
-                    self.parent[rx] -= 1
-                self.parent[ry] = rx
+                if self.parents[rx] == self.parents[ry]:
+                    self.parents[rx] -= 1
+                self.parents[ry] = rx
+                self.sizes[rx] += self.sizes[ry]
 
     def same(self, x, y):
         return self.root(x) == self.root(y)
 
+    def height(self, x):
+        return -self.parents[self.root(x)]
+
     def size(self, x):
-        return -self.parent[self.root(x)]
+        return self.sizes[self.root(x)]
 
 
 class WeightedUnionFind:
     def __init__(self, N):
-        self.parent = [-1] * N
+        self.parents = [-1] * N
         self.potential = [0] * N
         self.unites = N
 
     def root(self, x):
-        while self.parent[x] >= 0:
-            x = self.parent[x]
+        while self.parents[x] >= 0:
+            x = self.parents[x]
         return x
 
     def unite(self, x, y, w):
@@ -46,26 +52,26 @@ class WeightedUnionFind:
         if rx != ry:
             self.unites -= 1
             w = w + self.weight(x) - self.weight(y)
-            if self.parent[rx] > self.parent[ry]:
-                self.parent[rx] = ry
+            if self.parents[rx] > self.parents[ry]:
+                self.parents[rx] = ry
                 self.potential[rx] = -w
             else:
-                if self.parent[rx] == self.parent[ry]:
-                    self.parent[rx] -= 1
-                self.parent[ry] = rx
+                if self.parents[rx] == self.parents[ry]:
+                    self.parents[rx] -= 1
+                self.parents[ry] = rx
                 self.potential[ry] = w
 
     def same(self, x, y):
         return self.root(x) == self.root(y)
 
     def size(self, x):
-        return -self.parent[self.root(x)]
+        return -self.parents[self.root(x)]
 
     def weight(self, x):
         w = 0
-        while self.parent[x] >= 0:
+        while self.parents[x] >= 0:
             w += self.potential[x]
-            x = self.parent[x]
+            x = self.parents[x]
         return w
 
     def diff(self, x, y):
