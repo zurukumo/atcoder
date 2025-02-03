eaccc3397f3d8a6792db28ba6fb1ae3adad7b07b
zurukumo <zurukumo@gmail.com>
2025-02-04 00:52:58 +0900

    fix: データ微修正

--- abc003/d.py
+++ abc003/d.py
@@ -2,18 +2,20 @@ R, C = map(int, input().split())
 X, Y = map(int, input().split())
 D, L = map(int, input().split())
 
-mod = 10 ** 9 + 7
+mod = 10**9 + 7
 
 fac = [1]
 inv = [1]
-for i in range(1, R * C + 1) :
-  fac.append(fac[-1] * i % mod)
-  inv.append(pow(fac[-1], mod - 2, mod))
+for i in range(1, R * C + 1):
+    fac.append(fac[-1] * i % mod)
+    inv.append(pow(fac[-1], mod - 2, mod))
 
-def comb(n, r) :
-  if n < 0 or n < r :
-    return 0
-  return fac[n] * inv[n-r] * inv[r] % mod
+
+def comb(n, r):
+    if n < 0 or n < r:
+        return 0
+    return fac[n] * inv[n - r] * inv[r] % mod
+
 
 P1 = comb(X * Y, D) * comb(X * Y - D, L)
 P2 = comb((X - 1) * Y, D) * comb((X - 1) * Y - D, L) * 2
@@ -23,10 +25,12 @@ P5 = comb(X * (Y - 2), D) * comb(X * (Y - 2) - D, L)
 P6 = comb((X - 1) * (Y - 1), D) * comb((X - 1) * (Y - 1) - D, L) * 4
 P7 = comb((X - 2) * (Y - 1), D) * comb((X - 2) * (Y - 1) - D, L) * 2
 P8 = comb((X - 1) * (Y - 2), D) * comb((X - 1) * (Y - 2) - D, L) * 2
-if X == Y == 1 :
-  P9 = 0
-else :
-  P9 = comb((X - 2) * (Y - 2), D) * comb((X - 2) * (Y - 2) - D, L)
+if X == Y == 1:
+    P9 = 0
+else:
+    P9 = comb((X - 2) * (Y - 2), D) * comb((X - 2) * (Y - 2) - D, L)
 
-ret = (R - X + 1) * (C - Y + 1) * (P1 - (P2 + P3) + (P4 + P5 + P6) - (P7 + P8) + P9) % mod
+ret = (
+    (R - X + 1) * (C - Y + 1) * (P1 - (P2 + P3) + (P4 + P5 + P6) - (P7 + P8) + P9) % mod
+)
 print(ret)
