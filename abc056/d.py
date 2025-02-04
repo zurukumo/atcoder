22f1d6aab154427d8ddc2d35c644b1121b320109
zurukumo <zurukumo@gmail.com>
2025-02-04 16:06:36 +0900

    feat: 精進

--- abc056/d.py
+++ abc056/d.py
@@ -1,12 +1,35 @@
 import sys
+from bisect import bisect_left
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
-N = int(input())
-S = input()
 N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+a = [int(i) for i in input().split()]
+a.sort()
+a = a[: bisect_left(a, K)]
+N = len(a)
+
+
+def judge(n):
+    dp = [0] * K
+    dp[0] = 1
+    for i in range(N):
+        if i == n:
+            continue
+        for j in range(K - a[i] - 1, -1, -1):
+            if not dp[j]:
+                continue
+            if K - a[n] <= j + a[i]:
+                return True
+            dp[j + a[i]] = 1
+    return False
+
+
+l, r = -1, N
+while r - l > 1:
+    m = (l + r) // 2
+    if judge(m):
+        r = m
+    else:
+        l = m
+print(r)
--- abc091/d.py
+++ abc091/d.py
@@ -1,12 +1,33 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
+from bisect import bisect_left
 
 N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+a = [int(i) for i in input().split()]
+b = [int(i) for i in input().split()]
+
+ret = 0
+c = 1 << 28
+for i in range(28, -1, -1):
+    for j in range(N):
+        a[j] &= (1 << (i + 1)) - 1
+        b[j] &= (1 << (i + 1)) - 1
+    a.sort()
+    b.sort()
+
+    bit = 0
+    k, l, m, n = N - 1, N - 1, N - 1, N - 1
+    for j in range(N):
+        while k > 0 and a[k] > 1 * c - a[j]:
+            k -= 1
+        while l > 0 and a[l] > 2 * c - a[j]:
+            l -= 1
+        while m > 0 and a[m] > 3 * c - a[j]:
+            m -= 1
+        while n > 0 and a[n] > 4 * c - a[j]:
+            n -= 1
+        bit ^= (bisect_left(b, 2 * c - a[j]) - bisect_left(b, 1 * c - a[j])) & 1
+        bit ^= (bisect_left(b, 4 * c - a[j]) - bisect_left(b, 3 * c - a[j])) & 1
+    ret <<= 1
+    ret |= bit
+    c >>= 1
+
+print(ret)
