222b112fa93b0e7dbd46fe28fa6a1fe276a46bad
zurukumo <zurukumo@gmail.com>
2025-02-04 20:55:08 +0900

    feat: runコマンドを追加

---
+++ clis/commands/run.py
@@ -0,0 +1,18 @@
+import argparse
+import subprocess
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("contest", type=str, help="The contest to test")
+    parser.add_argument("problem", type=str, help="The problem to test")
+
+    args = parser.parse_args()
+
+    subprocess.run(
+        ["python3", f"{args.contest}/{args.problem}.py"],
+    )
+
+
+if __name__ == "__main__":
+    main()
