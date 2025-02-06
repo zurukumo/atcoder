e5a9e7285332d7ffbb77e938a887359660a65aea
zurukumo <zurukumo@gmail.com>
2025-02-06 14:15:36 +0900

    feat: 精進

--- arc142/c.py
+++ arc142/c.py
@@ -1,12 +1,32 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-
 N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+
+one = [-1] * (N + 1)
+two = [-1] * (N + 1)
+for i in range(3, N + 1):
+    print(f"? 1 {i}")
+    d = int(input())
+    one[i] = d
+    print(f"? 2 {i}")
+    d = int(input())
+    two[i] = d
+
+# 通常の場合
+ret = float("inf")
+sum3 = []
+for i in range(3, N + 1):
+    ret = min(ret, one[i] + two[i])
+    if one[i] + two[i] == 3:
+        sum3.append(i)
+
+# sum3の長さが0でも2でもなければ1と2は隣り合っている
+if len(sum3) != 0 and len(sum3) != 2:
+    ret = 1
+
+# sum3の長さが2でも、その2つが隣り合っていなければ1と2は隣り合っている
+if len(sum3) == 2:
+    print(f"? {sum3[0]} {sum3[1]}")
+    d = int(input())
+    if d != 1:
+        ret = 1
+
+print(f"! {ret}")
