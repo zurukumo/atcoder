a8a077964980d4d95cb23491a2953ffae876fda0
zurukumo <zurukumo@gmail.com>
2025-02-07 22:09:05 +0900

    feat: 精進

--- abc366/a.py
+++ abc366/a.py
@@ -1,12 +1,6 @@
-import sys
+N, T, A = map(int, input().split())
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-
-N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+if 2 * T > N or 2 * A > N:
+    print("Yes")
+else:
+    print("No")
--- arc119/c.py
+++ arc119/c.py
@@ -1,12 +1,20 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
+import collections
 
 N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+A = [int(i) for i in input().split()]
+
+mem = collections.defaultdict(int)
+s = 0
+mem[0] = 1
+for i, a in enumerate(A):
+    if i % 2 == 0:
+        s += a
+    else:
+        s -= a
+
+    mem[s] += 1
+
+ret = 0
+for k, v in mem.items():
+    ret += v * (v - 1) // 2
+print(ret)
