d10919c0d3928d83d3c82a8a572c9e0b97ecb927
zurukumo <zurukumo@gmail.com>
2025-02-15 20:56:42 +0900

    feat: 精進

--- contests/abc326/d.py
+++ contests/abc326/d.py
@@ -1,12 +1,52 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-
 N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+R = input()
+C = input()
+
+
+T = [["."] * N for _ in range(N)]
+col = [set() for _ in range(N)]
+row = [set() for _ in range(N)]
+
+
+def dfs(y, x, s):
+    if y == N:
+        if s == 3 * N:
+            print("Yes")
+            for t in T:
+                print("".join(t))
+            exit()
+        else:
+            return
+
+    if y > 0 and len(row[y - 1]) != 3:
+        return
+
+    candidates = set(["A", "B", "C"])
+    if len(row[y]) == 0:
+        candidates &= set(R[y])
+    if len(row[y]) > 0:
+        candidates -= row[y]
+    if len(col[x]) == 0:
+        candidates &= set(C[x])
+    if len(col[x]) > 0:
+        candidates -= col[x]
+
+    ny, nx = y, x + 1
+    if nx == N:
+        nx = 0
+        ny += 1
+
+    for c in candidates:
+        T[y][x] = c
+        row[y].add(c)
+        col[x].add(c)
+        dfs(ny, nx, s + 1)
+        row[y].remove(c)
+        col[x].remove(c)
+        T[y][x] = "."
+    dfs(ny, nx, s)
+
+
+dfs(0, 0, 0)
+
+print("No")
--- contests/abc336/b.py
+++ contests/abc336/b.py
@@ -1,12 +1,8 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-
 N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+
+ret = 0
+bit = list(bin(N))
+while bit[-1] == "0":
+    bit.pop()
+    ret += 1
+print(ret)
