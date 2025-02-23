657395a73eb5266e2cef73d0a927acf20593ead9
zurukumo <zurukumo@gmail.com>
2025-02-23 22:23:49 +0900

    fix: After Contestで落ちたので修正

--- contests/abc394/e.py
+++ contests/abc394/e.py
@@ -1,37 +1,40 @@
 import collections
-import heapq
 
 N = int(input())
 C = [input() for _ in range(N)]
 
-G = [[] for _ in range(N)]
+nG = [[] for _ in range(N)]
 rG = [[] for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if C[i][j] == "-":
             continue
-        G[i].append((j, C[i][j]))
+        nG[i].append((j, C[i][j]))
         rG[j].append((i, C[i][j]))
 
-queue = [(0, (i, i)) for i in range(N)]
-ret = collections.defaultdict(lambda: float("inf"))
+queue = collections.deque([])
+ret = [[float("inf")] * N for _ in range(N)]
 for i in range(N):
-    ret[(i, i)] = 0
-for i in range(N):
+    ret[i][i] = 0
+    queue.append((0, i, i))
     for j in range(N):
         if C[i][j] != "-":
-            ret[(i, j)] = min(ret[(i, j)], 1)
-            heapq.heappush(queue, (1, (i, j)))
+            if i != j:
+                ret[i][j] = 1
+                queue.append((1, i, j))
 
 while queue:
-    cost, v = heapq.heappop(queue)
-    f1, t1 = v
+    cost, f1, t1 = queue.popleft()
+    if cost > ret[f1][t1]:
+        continue
+
     for f2, c1 in rG[f1]:
-        for t2, c2 in G[t1]:
-            if c1 == c2:
-                if ret[(f2, t2)] > cost + 2:
-                    ret[(f2, t2)] = cost + 2
-                    heapq.heappush(queue, (cost + 2, (f2, t2)))
+        for t2, c2 in nG[t1]:
+            if c1 != c2:
+                continue
+            if cost + 2 < ret[f2][t2]:
+                ret[f2][t2] = cost + 2
+                queue.append((cost + 2, f2, t2))
 
 for i in range(N):
-    print(*[-1 if ret[(i, j)] == float("inf") else ret[(i, j)] for j in range(N)])
+    print(*[-1 if ret[i][j] == float("inf") else ret[i][j] for j in range(N)])
