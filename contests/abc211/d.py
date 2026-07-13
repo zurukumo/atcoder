513a192d77c59f0f1ccf080a4ba3ee890104c965
zurukumo <zurukumo@gmail.com>
2026-07-14 00:33:35 +0900

    feat: 精進

--- contests/abc211/d.py
+++ contests/abc211/d.py
@@ -1,12 +1,33 @@
+import heapq
 import sys
 
 input = sys.stdin.readline
 sys.setrecursionlimit(10**7)
 
-N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+N, M = map(int, input().split())
+AB = [[int(i) for i in input().split()] for _ in range(M)]
+mod = 10**9 + 7
+
+vec = [[] for _ in range(N)]
+for a, b in AB:
+    a -= 1
+    b -= 1
+    vec[a].append(b)
+    vec[b].append(a)
+
+
+dist = [(float("inf"), 0) for _ in range(N)]
+dist[0] = (0, 1)
+queue = [(0, 0)]
+while queue:
+    ccost, cur = heapq.heappop(queue)
+    ncost = ccost + 1
+    for nex in vec[cur]:
+        if dist[nex][0] == ncost:
+            npattern = (dist[nex][1] + dist[cur][1]) % mod
+            dist[nex] = (dist[nex][0], npattern)
+        elif ncost < dist[nex][0]:
+            dist[nex] = (ncost, dist[cur][1])
+            heapq.heappush(queue, (ncost, nex))
+
+print(dist[-1][1])
--- contests/abc212/b.py
+++ contests/abc212/b.py
@@ -1,12 +1,17 @@
-import sys
+X = [int(i) for i in input()]
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
-N = int(input())
-S = input()
-N, K = map(int, input().split())
-xy = [[int(i) for i in input().split()] for _ in range(N)]
-x = [int(i) for i in input().split()]
-S = [input() for _ in range(N)]
-A = [int(input()) for _ in range(N)]
+def check(s):
+    if s[0] == s[1] == s[2] == s[3]:
+        return True
+
+    if (s[1] - s[0]) % 10 == (s[2] - s[1]) % 10 == (s[3] - s[2]) % 10 == 1:
+        return True
+
+    return False
+
+
+if check(X):
+    print("Weak")
+else:
+    print("Strong")
